cc7c4013dea6a954b69d33aeb7ae84c637263dd9
Genesio S Pacheco <genesio.s.p.28@gmail.com>
2023-04-12 17:52:24 -0300

    feat: add contact component

---
+++ src/components/Contact/Contact.style.tsx
@@ -0,0 +1,14 @@
+import { createStyles } from "@mantine/core"
+
+const useStyles = createStyles(theme => ({
+  root: {
+    minHeight: "100vh",
+    paddingTop: "10vh",
+    color: theme.colors.dark[0]
+  },
+  span: {
+    color: "#99ee6f"
+  }
+}))
+
+export default useStyles
---
+++ src/components/Contact/Contact.tsx
@@ -0,0 +1,104 @@
+import {
+  TextInput,
+  Textarea,
+  SimpleGrid,
+  Group,
+  Title,
+  Button,
+  Container
+} from "@mantine/core"
+import { useForm } from "@mantine/form"
+import emailjs from "@emailjs/browser"
+
+import useStyles from "./Contact.style"
+
+import { env } from "@/env.mjs"
+
+const Contact = () => {
+  const form = useForm({
+    initialValues: {
+      name: "",
+      email: "",
+      subject: "",
+      message: ""
+    },
+    validate: {
+      name: value => value.trim().length < 2,
+      email: value => !/^\S+@\S+$/.test(value),
+      subject: value => value.trim().length === 0
+    }
+  })
+
+  emailjs.init(env.NEXT_PUBLIC_EMAILJS)
+
+  const { classes } = useStyles()
+  return (
+    <section className={classes.root} id="contact">
+      <Container>
+        <Title>
+          <span className={classes.span}>&lt;</span>Contact
+          <span className={classes.span}>&gt;</span>
+        </Title>
+        <form
+          onSubmit={form.onSubmit(formData => {
+            emailjs
+              .send("Portfólio", "Portfolio", formData, env.NEXT_PUBLIC_EMAILJS)
+              .then(res => console.log("SUCCESS!!", res.status, res.text))
+              .catch(err => console.error("FAILED...", err))
+          })}
+        >
+          <SimpleGrid
+            cols={2}
+            mt="xl"
+            breakpoints={[{ maxWidth: "sm", cols: 1 }]}
+          >
+            <TextInput
+              label="Nome"
+              placeholder="Seu Nome"
+              name="name"
+              variant="filled"
+              {...form.getInputProps("name")}
+            />
+            <TextInput
+              label="Email"
+              placeholder="Seu Email"
+              name="email"
+              variant="filled"
+              {...form.getInputProps("email")}
+            />
+          </SimpleGrid>
+          <TextInput
+            label="Assunto"
+            placeholder="Assunto"
+            mt="md"
+            name="subject"
+            variant="filled"
+            {...form.getInputProps("subject")}
+          />
+          <Textarea
+            label="Message"
+            placeholder="Sua mensagem"
+            mt="md"
+            maxRows={10}
+            minRows={5}
+            autosize
+            name="message"
+            variant="filled"
+            {...form.getInputProps("message")}
+          />
+          <Group position="center" mt="xl">
+            <Button type="submit" size="md">
+              Enviar
+            </Button>
+          </Group>
+        </form>
+        <Title>
+          <span className={classes.span}>&lt;/</span>Contact
+          <span className={classes.span}>&gt;</span>
+        </Title>
+      </Container>
+    </section>
+  )
+}
+
+export default Contact
